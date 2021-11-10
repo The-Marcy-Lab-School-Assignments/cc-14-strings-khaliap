fa5d6a38f620ab4409401adc58ac63ab666051d5
khalia <khaliaparris01@gmail.com>
2021-11-10 15:45:15 +0000

    finished code challenge

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //write your code here
+function stripUpperCase(string){
+    let lowerCase = "";
+    let lowerCaseLetter;
+    for(let i = 0; i < string.length; i++){
+        // lowerCaseLetter = string.charAt();
+      if(string[i] !== string[i].toUpperCase()){
+         lowerCase += string[i];
+      }
+    }
+    return lowerCase;
+}
+console.log(stripUpperCase(`HeLlo`));
